76099d628133bf73e0e64c41e6fa86f8cbaa8e82
donxuiqote <endinrhmnd@gmail.com>
2024-09-02 10:13:33 +0700

    a

---
+++ includes/navbar.php
@@ -0,0 +1,33 @@
+<nav class="navbar">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">
+            <img src="https://picsum.photos/50/50" alt="Logo Sekolah" class="logo">
+        </a>
+        <ul class="navbar-menu">
+            <li class="navbar-item"><a href="index.php" class="navbar-link">Beranda</a></li>
+            <li class="navbar-item dropdown">
+                <a href="#" class="navbar-link">Profil Sekolah</a>
+                <ul class="dropdown-menu">
+                    <li class="dropdown-item"><a href="dkv.php" class="dropdown-link">DKV</a></li>
+                    <li class="dropdown-item"><a href="usaha-layanan-masyarakat.php" class="dropdown-link">Usaha Layanan Masyarakat</a></li>
+                </ul>
+            </li>
+            <li class="navbar-item dropdown">
+                <a href="#" class="navbar-link">Kompetensi Keahlian</a>
+                <ul class="dropdown-menu">
+                    <li class="dropdown-item"><a href="dkv.php" class="dropdown-link">DKV</a></li>
+                    <li class="dropdown-item"><a href="usaha-layanan-masyarakat.php" class="dropdown-link">Usaha Layanan Masyarakat</a></li>
+                </ul>
+            </li>
+            <li class="navbar-item dropdown">
+                <a href="#" class="navbar-link">Info</a>
+                <ul class="dropdown-menu">
+                    <li class="dropdown-item"><a href="dkv.php" class="dropdown-link">DKV</a></li>
+                    <li class="dropdown-item"><a href="usaha-layanan-masyarakat.php" class="dropdown-link">Usaha Layanan Masyarakat</a></li>
+                </ul>
+            </li>
+            <li class="navbar-item"><a href="contact.php" class="navbar-link">Galeri</a></li>
+            <li class="navbar-item"><a href="contact.php" class="navbar-link">Kontak</a></li>
+        </ul>
+    </div>
+</nav>
